2a925c3651fb0eee253baa64cbfeb49d8119aafb
LeonOliva <leon.oliva2003@gmail.com>
2024-12-13 11:14:09 +0100

    Movimento 3D implementato
    Prima non mi ha salvato le modifiche

--- Assets/Scripts/Session/SessionPlayer.cs
+++ Assets/Scripts/Session/SessionPlayer.cs
@@ -3,18 +3,35 @@ using UnityEngine;
 
 public class SessionPlayer : NetworkBehaviour
 {
+<<<<<<< Updated upstream
     //Variabili serializzate per impostare la velocità di movimento del giocatore e il renderer del mesh.
+=======
+    //Variabili serializzate per impostare la velocit� di movimento del giocatore e il renderer del mesh.
+>>>>>>> Stashed changes
     [SerializeField] private float moveSpeed = 2;
     [SerializeField] private MeshRenderer meshRenderer = null;
 
     //Variabili private per la gestione del controller del personaggio, dell'ID e del colore del giocatore.
-    private CharacterController controller = null;
+    CharacterController controller;
     private string _colorHex = "";
     private string _id = "";
+<<<<<<< Updated upstream
     
     private Rigidboy characterRigidbody;
     private PlayerInput playerInput;
     prvate PlayerInputActions playerInputActions;
+=======
+    float velocità;
+    float velocità_walk = 5f;
+    float velocità_run = 10f;
+    public Transform TerraCheck;
+    float distanzaTerra = 1f;
+    public LayerMask TerraMask; //componente che ci dice quando il player tocca il terreno (momento nel quale "disattivo" la forza di gravità)
+    bool toccaterra;
+    Vector3 velocitày;
+    float gravità = -9.8f;
+    float altezzasalto = 1f;
+>>>>>>> Stashed changes
 
     private void Awake() //Metodo chiamato all'inizio della vita dell'oggetto per inizializzare il controller.
     {
@@ -60,10 +77,43 @@ public class SessionPlayer : NetworkBehaviour
         PlayerInputActions
         if (IsOwner)
         {
-            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            controller.Move(new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime * moveSpeed);
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            Vector3 movimento = transform.right * x + transform.forward * z; //direzione che daremo al player
+ 
+            if(movimento!=Vector3.zero)
+            {
+                velocità = velocità_walk;
+
+                if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    velocità = velocità_run;
+                } 
+           
+                controller.Move(movimento * Time.deltaTime * velocità);
+            } 
+            
+            toccaterra = Physics.CheckSphere(TerraCheck.position, distanzaTerra, TerraMask); //funzione che ritorna "true" se collide altrimenti "false"    
+            
+            if(toccaterra && velocitày.y < 0)
+            {
+                velocitày.y = -10f;
+            } 
+       
+            if(Input.GetButtonDown("Jump") && toccaterra)
+            {
+                velocitày.y = Mathf.Sqrt(gravità * altezzasalto * -2f);
+            }
+       
+            velocitày.y += gravità * Time.deltaTime;
+            controller.Move(velocitày * Time.deltaTime);
         }
+<<<<<<< Updated upstream
     }*/
+=======
+
+    }
+>>>>>>> Stashed changes
     
 
     [Rpc(SendTo.Everyone)] //Metodo chiamato tramite RPC per applicare l'ID e il colore del giocatore. Destinato a tutti i client.
