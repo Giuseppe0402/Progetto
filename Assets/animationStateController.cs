bfbb9e5aae04909b7529923e263d961eb4b7d7db
zizziz03 <poulgigi839@gmail.com>
2024-12-14 20:13:21 +0100

    Aggiustando modifiche
    Ho rimosso lo script AnimationStateControllerX e Y per crearne uno solo, sia perché mi sono accorto che fosse inutile averne due diversi e sia perché in realtà quello Y mi dava problemi, non so per quale motivo, quindi ho deciso di crearne uno unico di script per Y e XBot

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animationStateControllerX : MonoBehaviour
+public class animationStateController : MonoBehaviour
 {
     Animator animator;
     int isWalkingHash;
